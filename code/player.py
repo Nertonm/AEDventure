9624650acef6efd006b064d67b39a1e8cd1308a4
nertonm <thiago2nerton@gmail.com>
2024-10-09 21:33:06 -0300

    Movendo a constante player speed para settings

--- code/player.py
+++ code/player.py
@@ -1,17 +1,14 @@
 import pygame
 from settings import *
 
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png')
         self.rect = self.image.get_rect(topleft = (pos))
         self.hitbox = self.rect.inflate(0, -26)
-
         self.direction = pygame.math.Vector2()
-        self.speed = 5
-
+        self.speed = SPEED
         self.obstacle_sprites = obstacle_sprites
 
     def input(self):
