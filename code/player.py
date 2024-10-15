7c4568e8c2debae7594ad17f3a0ebf2721725325
nertonm <thiago2nerton@gmail.com>
2024-10-15 04:41:03 -0300

    implementando awsd como forma de movimento

--- code/player.py
+++ code/player.py
@@ -39,19 +39,19 @@ class Player(pygame.sprite.Sprite):
 			keys = pygame.key.get_pressed()
 
 			# movement input
-			if keys[pygame.K_UP]:
+			if keys[pygame.K_UP] or keys[pygame.K_w]:
 				self.direction.y = -1
 				self.status = 'up'
-			elif keys[pygame.K_DOWN]:
+			elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
 				self.direction.y = 1
 				self.status = 'down'
 			else:
 				self.direction.y = 0
 
-			if keys[pygame.K_RIGHT]:
+			if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
 				self.direction.x = 1
 				self.status = 'right'
-			elif keys[pygame.K_LEFT]:
+			elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
 				self.direction.x = -1
 				self.status = 'left'
 			else:
