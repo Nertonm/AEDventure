acb57d50c0142190595e3a40250889f45bc7a961
nertonm <thiago2nerton@gmail.com>
2024-10-20 20:20:08 -0300

    aaa

---
+++ map/TX Propsa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TX Propsa" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="../graphics/Texture/TX Props.png" width="512" height="512"/>
+</tileset>
