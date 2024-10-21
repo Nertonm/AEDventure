0ce5d8090a2d15e142254722bc5265a02497a332
Nerton <thiago2nerton@gmail.com>
2024-10-21 11:00:54 -0300

    deleting files

--- map/TX PropsBig.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="TX PropsBig" tilewidth="256" tileheight="256" tilecount="4" columns="2">
- <image source="../graphics/Texture/TX Props.png" width="512" height="512"/>
-</tileset>
