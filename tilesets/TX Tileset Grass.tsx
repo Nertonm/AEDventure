31f0d0e9fa89292543364db0e8d893c702cd629d
Nerton <thiago2nerton@gmail.com>
2024-10-21 11:17:40 -0300

    deletando arquivos

--- tilesets/TX Tileset Grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="TX Tileset Grass" tilewidth="64" tileheight="64" tilecount="16" columns="4">
- <image source="../graphics/Texture/TX Tileset Grass.png" width="256" height="256"/>
-</tileset>
