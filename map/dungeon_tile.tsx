4c4287d8774e2628cce5a4529ac21a62b5a6b2b0
Nerton <thiago2nerton@gmail.com>
2024-10-21 12:00:06 -0300

    adcionando graficos

---
+++ map/dungeon_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="dungeon_tile" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <image source="../graphics/dungeon_tile.png" width="640" height="640"/>
+</tileset>
