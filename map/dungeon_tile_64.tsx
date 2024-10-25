c7ed9edf11950e73996a5adfd362503e00104751
nertonm <thiago2nerton@gmail.com>
2024-10-24 22:48:32 -0300

    a

---
+++ map/dungeon_tile_64.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="dungeon_tile" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <grid orientation="orthogonal" width="128" height="128"/>
+ <image source="../graphics/dungeon_tile.png" width="640" height="640"/>
+</tileset>
