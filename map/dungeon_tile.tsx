396194feb1bb6362257029a6962ea0f88fef3b92
Luana Teles <142579091+lua-teles@users.noreply.github.com>
2024-10-31 11:19:14 -0300

    new map

--- map/dungeon_tile.tsx
+++ map/dungeon_tile.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="dungeon_tile" tilewidth="128" tileheight="128" tilecount="100" columns="10">
- <image source="../graphics/dungeon_tile_128.png" width="1280" height="1280"/>
+<tileset version="1.10" tiledversion="1.11.0" name="dungeon_tile" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <image source="../graphics/dungeon_tile.png" width="640" height="640"/>
 </tileset>
