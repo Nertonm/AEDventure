ee554be52bd0e8867116fc4db4ee276729d7b2bf
vic <vitoriapontes044@gmail.com>
2024-11-07 18:26:29 -0300

    aprimorando o menu do hanoi e adicionando botão de fechar

--- code/hanoi.py
+++ code/hanoi.py
@@ -1,4 +1,5 @@
 import pygame, sys, time
+import settings
 
 class Hanoi:
     def __init__(self, display_surface, end_challenge_callback, difficulty):
@@ -15,16 +16,19 @@ class Hanoi:
         self.floating = False
         self.floater = 0
         self.display_surface = display_surface
-        self.towers_midx = [display_surface.get_width() // 2 - 200, display_surface.get_width() // 2, display_surface.get_width() // 2 + 200]
+        # Posições das torres
+        self.towers_midx = [settings.WIDTH // 2 - 200, settings.WIDTH // 2, settings.WIDTH // 2 + 200]
         self.clock = pygame.time.Clock()
         self.end_challenge_callback = end_challenge_callback
 
-        # Posições das torres
-        self.towers_midx = [120, 320, 520]
-
         # Cores
-        self.white, self.grey, self.blue, self.green = (255, 255, 255), (170, 170, 170), (78, 162, 196), (77, 206, 145)
+        self.white, self.grey, self.violet, self.purple, self.black = (255, 255, 255), (170, 170, 170), (86, 55, 103), (54, 13, 45), (0, 0, 0)
 
+        # Botão para encerrar o desafio
+        button_width, button_height = 260, 75
+        button_x = (settings.WIDTH) // 2 - button_width // 2
+        button_y = settings.HEIGHT * 0.75 - button_height // 2  # Ajuste a posição Y conforme necessário
+        self.close_button = pygame.Rect(button_x, button_y, button_width, button_height)
 
     def make_disks(self):
         self.disks = []
@@ -37,26 +41,27 @@ class Hanoi:
 
     def draw_towers(self):
         for xpos in self.towers_midx:
-            pygame.draw.rect(self.display_surface, self.green, pygame.Rect(xpos - 80, 400, 160, 20))
+            pygame.draw.rect(self.display_surface, self.purple, pygame.Rect(xpos - 80, 400, 160, 20))
             pygame.draw.rect(self.display_surface, self.grey, pygame.Rect(xpos - 5, 200, 10, 200))
 
     def draw_disks(self):
         for disk in self.disks:
-            pygame.draw.rect(self.display_surface, self.blue, disk['rect'])
+            pygame.draw.rect(self.display_surface, self.violet, disk['rect'])
 
     def draw_ptr(self):
         pygame.draw.polygon(self.display_surface, (255, 0, 0), [(self.towers_midx[self.pointing_at]-7 ,440), (self.towers_midx[self.pointing_at]+7, 440), (self.towers_midx[self.pointing_at], 433)])
+
+    def draw_close_button(self):
+        pygame.draw.rect(self.display_surface, self.grey, self.close_button)
+        font = pygame.font.Font(settings.UI_FONT, settings.UI_FONT_SIZE_MENU)
+        text = font.render('Close', True, self.purple)
+        self.display_surface.blit(text, (self.close_button.x + 10, self.close_button.y + 5))
 
     def check_won(self):
         if all(disk['tower'] == 2 for disk in self.disks):
             time.sleep(0.2)
             self.end_challenge_callback()
             return True
-
-    #def reset():
-    #    global steps, pointing_at, floating, floater
-    #    steps, pointing_at, floating, floater = 0, 0, False, 0
-    #    make_disks()
 
     def start(self):
         self.make_disks()
@@ -100,15 +105,22 @@ class Hanoi:
                             self.disks[self.floater]['rect'].midtop = (self.towers_midx[self.pointing_at], 400 - 23)
                             self.steps += 1
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.close_button.collidepoint(event.pos):
+                        self.end_challenge_callback()
+                        running = False
+                        break
+
                 # Cria uma superfície semi-transparente
                 overlay = pygame.Surface((self.display_surface.get_width(), self.display_surface.get_height()))
                 overlay.fill((0, 0, 128))
                 self.display_surface.blit(overlay, (0, 0))
 
-                self.display_surface.fill(self.white)
+                self.display_surface.fill(self.black)
                 self.draw_towers()
                 self.draw_disks()
                 self.draw_ptr()
+                self.draw_close_button()
 
                 pygame.display.flip()
 
