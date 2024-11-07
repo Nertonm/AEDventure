c9b71f7dffde1b24ece516ce8ec2a78636732c53
nertonm <thiago2nerton@gmail.com>
2024-11-07 01:50:48 -0300

    terminando a issue

--- code/settings.py
+++ code/settings.py
@@ -1,10 +1,25 @@
 # game setup
+import pygame
 
 WIDTH    = 1280
 HEIGHT   = 720
 FPS      = 60
 TILESIZE = 128
-DIFFICULTY = hard  # easy, medium, hard
+DIFFICULTY = 'medium'  # 'easy', 'medium', or 'hard'
+# Key bindings
+MOVE_UP = pygame.K_UP
+MOVE_DOWN = pygame.K_DOWN
+MOVE_LEFT = pygame.K_LEFT
+MOVE_RIGHT = pygame.K_RIGHT
+ATTACK = pygame.K_SPACE
+SPRINT = pygame.K_LSHIFT
+
+MOVE_UP1 = pygame.K_w
+MOVE_DOWN1 = pygame.K_s
+MOVE_LEFT1 = pygame.K_a
+MOVE_RIGHT1 = pygame.K_d
+ATTACK1 = pygame.K_SPACE
+SPRINT1 = pygame.K_LSHIFT
 
 # Cores
 WHITE = (255, 255, 255)
