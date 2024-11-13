abfb3b40492a3d21f700acc0d9969bc9bce7ee86
vic <vitoriapontes044@gmail.com>
2024-11-13 14:12:12 -0300

    removido a lógica de ataque da jogadora

--- code/player.py
+++ code/player.py
@@ -18,75 +18,47 @@ class Player(pygame.sprite.Sprite):
 		# movement 
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
-		self.attacking = False
-		self.attack_cooldown = 400
-		self.attack_time = None
 
 		self.obstacle_sprites = obstacle_sprites
 
 	def import_player_assets(self):
 		character_path = '../graphics/player/'
 		self.animations = {'up': [],'down': [],'left': [],'right': [],
-			'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
-			'right_attack':[],'left_attack':[],'up_attack':[],'down_attack':[]}
+			'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[]}
 
 		for animation in self.animations.keys():
 			full_path = character_path + animation
 			self.animations[animation] = import_folder(full_path)
 
 	def input(self):
-		if not self.attacking:
-			keys = pygame.key.get_pressed()
+		keys = pygame.key.get_pressed()
 
-			# movement input
-			if keys[MOVE_UP] or keys[MOVE_UP1]:
-				self.direction.y = -1
-				self.status = 'up'
-			elif keys[MOVE_DOWN] or keys[MOVE_DOWN1]:
-				self.direction.y = 1
-				self.status = 'down'
-			else:
-				self.direction.y = 0
+		# movement input
+		if keys[MOVE_UP] or keys[MOVE_UP1]:
+			self.direction.y = -1
+			self.status = 'up'
+		elif keys[MOVE_DOWN] or keys[MOVE_DOWN1]:
+			self.direction.y = 1
+			self.status = 'down'
+		else:
+			self.direction.y = 0
 
-			if keys[MOVE_RIGHT] or keys[MOVE_RIGHT1]:
-				self.direction.x = 1
-				self.status = 'right'
-			elif keys[MOVE_LEFT] or keys[MOVE_LEFT1]:
-				self.direction.x = -1
-				self.status = 'left'
-			else:
-				self.direction.x = 0
-
-			# attack input
-			if keys[ATTACK] or keys[ATTACK1]:
-				self.attacking = True
-				self.attack_time = pygame.time.get_ticks()
-				print('attack')
-
-			# magic input
-			#if keys[MAGIC] or keys[MAGIC1]:
-			#	self.attacking = True
-			#	self.attack_time = pygame.time.get_ticks()
-			#	print('magic')
+		if keys[MOVE_RIGHT] or keys[MOVE_RIGHT1]:
+			self.direction.x = 1
+			self.status = 'right'
+		elif keys[MOVE_LEFT] or keys[MOVE_LEFT1]:
+			self.direction.x = -1
+			self.status = 'left'
+		else:
+			self.direction.x = 0
 
 	def get_status(self):
 
 		# idle status
 		if self.direction.x == 0 and self.direction.y == 0:
-			if not 'idle' in self.status and not 'attack' in self.status:
+			if not 'idle' in self.status:
 				self.status = self.status + '_idle'
 
-		if self.attacking:
-			self.direction.x = 0
-			self.direction.y = 0
-			if not 'attack' in self.status:
-				if 'idle' in self.status:
-					self.status = self.status.replace('_idle','_attack')
-				else:
-					self.status = self.status + '_attack'
-		else:
-			if 'attack' in self.status:
-				self.status = self.status.replace('_attack','')
 
 	def move(self,speed):
 		if self.direction.magnitude() != 0:
@@ -119,10 +91,6 @@ class Player(pygame.sprite.Sprite):
 
 	def cooldowns(self):
 		current_time = pygame.time.get_ticks()
-
-		if self.attacking:
-			if current_time - self.attack_time >= self.attack_cooldown:
-				self.attacking = False
 
 	def animate(self):
 		animation = self.animations[self.status]
