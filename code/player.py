ddce8c899ceeeee22a88aa1d9f3bea9fbad42580
vic <vitoriapontes044@gmail.com>
2024-11-13 14:02:06 -0300

    mudança no sprite inicial da jogadora

--- code/player.py
+++ code/player.py
@@ -5,7 +5,7 @@ from support import import_folder
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,groups,obstacle_sprites):
 		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/player/personagem1.png').convert_alpha()
+		self.image = pygame.image.load('../graphics/player/down_idle/idle_down.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0, -26)
 
